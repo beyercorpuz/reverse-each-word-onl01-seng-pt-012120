4eb47e1082dcc101cccb278e34ede64f16be66cd
Chris Beyer <beyercorpuz@gmail.com>
2020-02-03 20:11:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,10 @@ def reverse_each_word(words)
 	words_array = words.split(' ')
 	reversed = words_array.collect{|item| item.reverse}
   result = reversed.join(' ')
+	return result
 	
 end
+  
   
 
   

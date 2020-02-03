c603b3b0cab3565e10ad74e4bf3e50838622979f
Chris Beyer <beyercorpuz@gmail.com>
2020-02-03 20:02:08 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word("The Superbowl Was Good")
+ end
+  
+
+  
+#   puts "The word reversed is #{word}"
+#   end
+#   word.collect do|word|
+#   puts " The word reversed is #{word}."
+# end
+# end
+
+
+
+
